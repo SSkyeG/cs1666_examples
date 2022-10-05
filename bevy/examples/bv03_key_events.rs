ebc81846d661c905f98d261a20c7ab51655e458e
SSkyeG <60981354+SSkyeG@users.noreply.github.com>
2022-10-05 12:26:35 -0400

    Did Bevy example 3
    example 3 done for team Jersey Shores

--- bevy/examples/bv03_key_events.rs
+++ bevy/examples/bv03_key_events.rs
@@ -19,11 +19,29 @@ fn main() {
 		.insert_resource(ClearColor(Color::CYAN))
 		.add_plugins(DefaultPlugins)
 		.add_startup_system(setup)
+		.add_system(keyboard_input)
 		.run();
 }
 
 fn setup(mut commands: Commands) {
 	commands.spawn_bundle(Camera2dBundle::default());
+}
+
+fn keyboard_input(
+		input: Res<Input<KeyCode>>, mut clear_color: ResMut<ClearColor>
+) {
+    if input.just_pressed(KeyCode::W) {
+		clear_color.0 = Color::RED;
+    }
+    else if input.just_pressed(KeyCode::S) {
+		clear_color.0 = Color::BLUE;
+    }
+	else if input.just_pressed(KeyCode::A) {
+		clear_color.0 = Color::GREEN;
+    }
+	else if input.just_pressed(KeyCode::D) {
+		clear_color.0 = Color::FUCHSIA;
+    }
 }
 
 //TODO: Write a system to change the clear color in response to a keypress
